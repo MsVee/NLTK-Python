0de2bf097870b6647c6d5add65a31d437e930121
Eric Coker <bears34egc@yahoo.com>
2015-01-04 00:00:04 -0600

    Update machinelearn.py
    Many changes. Decided to use Statsmodels and Scikit OLS for Retweet Count Prediction. As well as Decision Tree Regressor and Random Forests. Low R Squared on OLS, but Rainbow Test for Linearity was promising....

--- machinelearn.py
+++ machinelearn.py
@@ -22,9 +22,18 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import StandardScaler
 from sklearn.feature_extraction.text import HashingVectorizer
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.ensemble import RandomForestClassifier as RF
+from sklearn.metrics import roc_auc_score as AUC
 from sklearn.decomposition import PCA
+from sklearn import preprocessing
+from math import sqrt
+from sklearn import linear_model
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.externals.six import StringIO 
+from sklearn import tree
+from sklearn.cross_validation import cross_val_score
+from sklearn import metrics
 
 # class for debugging errors
 class MyObj(object):
@@ -49,23 +58,6 @@ train, test = train_test_split(twitter_df, test_size=.3, random_state=0)
 
 testing = pd.DataFrame(test, columns = ['index', 'follower_count', 'friend_count', 'geo', 'location', 'name', 'place', 'retweet_count', 'screen_name', 'source', 'status_id', 'status_text', 'time_zone', 'timestamp', 'surge_pricing', 'free_rides', 'promo', 'driver', 'food', 'controversy', 'regulations'])
 training = pd.DataFrame(train, columns = ['index', 'follower_count', 'friend_count', 'geo', 'location', 'name', 'place', 'retweet_count', 'screen_name', 'source', 'status_id', 'status_text', 'time_zone', 'timestamp', 'surge_pricing', 'free_rides', 'promo', 'driver', 'food', 'controversy', 'regulations'])
-# training = training.drop(['unnamed0', 'geo', 'name' 'place', 'screen_name', 'status_id'], axis=1)
-# testing = testing.drop(['unnamed0', 'geo', 'name' 'place', 'screen_name', 'status_id'], axis=1)
-# training.to_csv(dir + 'training.csv')
-# testing.to_csv(dir + 'testing.csv')
-# y, X = dmatrices('retweet_count ~ surge_pricing + free_rides + promo + driver + food + controversy + regulations', data=twitter_df, return_type='dataframe')
-# # Define the model from above Patsy-created variables, using Statsmodels
-# print sm.OLS(y,X).fit().summary()
-# print sm.OLS(y,X).fit().params
-# print 'r sqd is : ', sm.OLS(y,X).fit().rsquared
-# rainbow=sm.stats.linear_rainbow(sm.OLS(y,X).fit())
-# print 'Rainbow Test for Linearity is ', rainbow
-# y_hat, X_hat = dmatrices('retweet_count ~ surge_pricing + free_rides + promo + driver + food + controversy + regulations', data=testing, return_type='dataframe')
-# y_pred = sm.OLS(y,X).fit().predict(X)
-# twitter_df['retweet_pred'] = pd.Series(y_pred)
-
-# Apply decision tree and/or CART classification on Retweet Count
-model = DecisionTreeClassifier()
 training['follower_count_l'] = training['follower_count'].replace(0, 1e-6)
 training['follower_count_l'] = np.log(training['follower_count_l'])
 testing['follower_count_l'] = testing['follower_count'].replace(0, 1e-6)
@@ -74,58 +66,104 @@ training['friend_count_l'] = training['friend_count'].replace(0, 1e-6)
 training['friend_count_l'] = np.log(training['friend_count_l'])
 testing['friend_count_l'] = testing['friend_count'].replace(0, 1e-6)
 testing['friend_count_l'] = np.log(testing['friend_count_l'])
-numeric_cols = ['friend_count', 'follower_count']
-# X_num_tr = training[list(numeric_cols)].as_matrix().astype(np.float)
-# X_num_te = testing[list(numeric_cols)].as_matrix().astype(np.float)
-cat_cols = ['time_zone', 'timestamp', 'location', 'source', 'surge_pricing', 'free_rides', 'promo', 'driver', 'food', 'controversy', 'regulations']
-columns = cat_cols + numeric_cols
-# num_train = np.hstack((tr_foll, tr_friend))
-# num_test = np.hstack((te_foll, te_friend))
-# cattr = training[list(cat_cols)]
-# # cat_tr = cattr.T.to_dict().values()
-# catte = testing[list(cat_cols)]
-# cat_te = catte.T.to_dict().values()
-# vectorizer=DV(sparse=False)
-# categ_train = vectorizer.fit_transform(cat_tr)
-# categ_test = vectorizer.fit_transform(cat_te)
-feat_num = training[list(numeric_cols)]
-scaler = StandardScaler()
-numx = scaler.fit_transform(feat_num)
-features_cat = training[list(cat_cols)]
-X = np.hstack((numx + features_cat))
-t_features = {}
-t_features = X.to_dict
-from sklearn.feature_extraction import DictVectorizer
-vec = DictVectorizer()
-vector = vec.fit_transform(t_features).toarray()
-# features_x = sparse.csr_matrix(features.values)
-# imp = Imputer(missing_values = 'NaN', strategy='mean', axis=0)
-# imp.fit(categ_train)
-# categ_train = imp.transform(categ_train)
-# print categ_train.shape
-# print X_num_tr.shape
-# print categ_test.shape
-# print X_num_te.shape
-# features_train = np.hstack((categ_train, X_num_tr))
-# features_test = np.hstack((categ_test, X_num_te))
+le=preprocessing.LabelEncoder()
+training['time_zone_l'] = le.fit(training['time_zone'])
+testing['time_zone_l'] = le.fit(testing['time_zone'])
+training['location_l'] = le.fit(training['location'])
+testing['location_l'] = le.fit(testing['location'])
+training['source_l'] = le.fit(training['source'])
+testing['source_l'] = le.fit(testing['source'])
+training['retweet_count_l'] = np.log(training['retweet_count'].replace(0, 1e-6))
+testing['retweet_count_l'] = np.log(testing['retweet_count'].replace(0, 1e-6))
+
+y, X = dmatrices('retweet_count_l ~ surge_pricing + free_rides + promo + driver + food + controversy + regulations', data=training, return_type='dataframe')
+# Define the model from above Patsy-created variables, using Statsmodels
+print sm.OLS(y,X).fit().summary()
+print sm.OLS(y,X).fit().params
+print 'r sqd is : ', sm.OLS(y,X).fit().rsquared
+rainbow=sm.stats.linear_rainbow(sm.OLS(y,X).fit())
+print 'Rainbow Test for Linearity is ', rainbow
+y_hat, X_hat = dmatrices('retweet_count_l ~ surge_pricing + free_rides + promo + driver + food + controversy + regulations', data=testing, return_type='dataframe')
+y_pred = sm.OLS(y,X).fit().predict(X_hat)
+twitter_df['retweet_pred'] = pd.Series(y_pred)
+# plt.figure()
+# plt.plot(X,y,'o',X, y_pred, 'b-')
+
+# plt.plot(X, sm.fittedvalues, 'r--.')
+# plt.plot(X, iv_u, 'r--')
+# plt.plot(X, iv_l, 'r--')
+# plt.title('blue: true,   red: OLS')
+# plt.show()
+
+numeric_cols = ['friend_count_l', 'follower_count_l']
+x_num_train = training[numeric_cols].as_matrix()
+x_num_test = testing[numeric_cols].as_matrix()
+cat_cols = ['time_zone', 'location', 'source', 'surge_pricing', 'free_rides', 'promo', 'driver', 'food', 'controversy', 'regulations']
+cat_train = training[cat_cols]
+cat_test = testing[cat_cols]
+
+x_cat_train = cat_train.T.to_dict().values()
+x_cat_test = cat_test.T.to_dict().values()
+vectorizer = DV(sparse=False)
+vec_x_cat_train = vectorizer.fit_transform(x_cat_train)
+vec_x_cat_test = vectorizer.transform(x_cat_test)
+
+x_train = np.hstack((x_num_train, vec_x_cat_train))
+x_test = np.hstack((x_num_test, vec_x_cat_test))
+
+target_train = np.array(training['retweet_count_l'])
+target_test = np.array(testing['retweet_count_l'])
 
 
-# vectorizer=DV(sparse=True)
-# features_train = vectorizer.fit_transform(features_train)
-# features_test = vectorizer.fit_transform(features_test)
-# features_train = np.hstack((features_train, numtr))
-# features_test = np.hstack((features_test, numte))
-training['retweet_count'] = training['retweet_count'].replace(0, 1e-6)
-testing['retweet_count'] = testing['retweet_count'].replace(0, 1e-6)
-target_train = np.log((training['retweet_count']).values)
-target_test = np.log((testing['retweet_count']).values)
+print "training regression..."
+
+regr = linear_model.LinearRegression(normalize=True)
+# Train the model using the training sets
+regr.fit(x_train, target_train)
+
+# The coefficients
+print('Coefficients: \n', regr.coef_)
+# The mean square error
+print("Residual sum of squares: %.2f"
+      % np.mean((regr.predict(x_test) - target_test) ** 2))
+# Explained variance score: 1 is perfect prediction
+print('Variance score: %.2f' % regr.score(x_test, target_test))
+
+# The mean square error
+print 'mse ', np.mean((regr.predict(x_test)-target_test)**2)
 
 
-model.fit(vector, target_train)
-print(model)
-# make the predictions
-expected = target_train
-predicted = model.predict(vector)
-#check the fit of predictions
-print(metrics.classification_report(expected, predicted))
-print(metrics.confusion_matrix(expected, predicted))
+#Decision Tree Regression
+print(__doc__)
+clf = DecisionTreeRegressor(max_depth=7)
+clf.fit(x_train, target_train)
+
+y_fit_1 = clf.predict(x_test)
+print 'cross val score for DecisionTreeRegressor'
+print cross_val_score(clf, x_train, target_train, cv=10)
+# print "classification accuracy:", metrics.accuracy_score(target_test, y_fit_1)
+plt.figure()
+
+plt.plot(x_test, y_fit_1, c="r", label="max_depth=7", linewidth=2, alpha=.3)
+plt.xlabel("data")
+plt.ylabel("target")
+plt.title("Decision Tree Regression")
+plt.legend()
+plt.show()
+
+#Random Forest
+clf = RandomForestRegressor(n_estimators=200, max_depth=5, 
+                            min_samples_leaf=3)
+clf.fit(x_train, target_train)
+y_fit_200 = clf.predict(x_test)
+print 'cross val score for Random Forest'
+print cross_val_score(clf, x_train, target_train, cv=10)
+print "classification accuracy:", metrics.accuracy_score(target_test, y_fit_200)
+plt.figure()
+plt.plot(x_train, target_train, '.k', alpha=0.3)
+plt.xlabel("data")
+plt.ylabel("target")
+plt.title("Random Forest")
+plt.legend()
+plt.show()
+
